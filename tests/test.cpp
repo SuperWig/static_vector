d12063b97da62c5a7acc86ff41bbe62e7e2d6752
Daniel Marshall <xandan@gmail.com>
2021-10-26 07:57:55 +0100

    using namespace dpm;

--- tests/test.cpp
+++ tests/test.cpp
@@ -8,7 +8,9 @@
 #include <doctest/doctest.h>
 #include <dpm/static_vector.h>
 
-// static_assert(std::is_empty_v<dpm::static_vector<int, 0>>);
+using namespace dpm;
+
+// static_assert(std::is_empty_v<static_vector<int, 0>>);
 
 struct constructor_count
 {
@@ -54,7 +56,7 @@ TEST_CASE("constructors/destructors")
 {
     SUBCASE("static_vector(const static_vector&)")
     {
-        dpm::static_vector<copy_move_tester, 5> sv(2);
+        static_vector<copy_move_tester, 5> sv(2);
         sv[0] = 10;
         sv[1] = 20;
         auto copy = sv;
@@ -65,21 +67,21 @@ TEST_CASE("constructors/destructors")
     }
     SUBCASE("static_vector(static_vector&&)")
     {
-        dpm::static_vector<copy_move_tester, 2> sv(2);
+        static_vector<copy_move_tester, 2> sv(2);
         const auto address = sv[0].addr();
         auto moved_sv = std::move(sv);
         REQUIRE(address == moved_sv[0].addr());
     }
     SUBCASE("static_vector(size_type)")
     {
-        dpm::static_vector<constructor_count, 5> sv1(0);
+        static_vector<constructor_count, 5> sv1(0);
         REQUIRE(constructor_count::count == 0);
-        dpm::static_vector<constructor_count, 5> sv2(3);
+        static_vector<constructor_count, 5> sv2(3);
         REQUIRE(constructor_count::count == 3);
     }
     SUBCASE("static_vector(size_type, value_type)")
     {
-        dpm::static_vector<int, 3> sv(3, 2);
+        static_vector<int, 3> sv(3, 2);
         REQUIRE(sv.size() == 3);
         REQUIRE(sv[0] == 2);
         REQUIRE(sv[1] == 2);
@@ -88,7 +90,7 @@ TEST_CASE("constructors/destructors")
     SUBCASE("static_vector(InputIter, InputIter)")
     {
         std::array<int, 3> test{ 1, 2, 3 };
-        dpm::static_vector<int, 3> sv(test.begin(), test.end());
+        static_vector<int, 3> sv(test.begin(), test.end());
 
         REQUIRE(sv[0] == 1);
         REQUIRE(sv[1] == 2);
@@ -97,8 +99,8 @@ TEST_CASE("constructors/destructors")
     SUBCASE("~static_vector()")
     {
         {
-            dpm::static_vector<constructor_count, 3> sv1(3);
-            dpm::static_vector<constructor_count, 3> sv2(1);
+            static_vector<constructor_count, 3> sv1(3);
+            static_vector<constructor_count, 3> sv2(1);
         }
         REQUIRE(constructor_count::count == 0);
     }
@@ -108,8 +110,8 @@ TEST_CASE("assignment")
 {
     SUBCASE("copy")
     {
-         dpm::static_vector<copy_move_tester, 3> sv1{ 1, 3 };
-         dpm::static_vector<copy_move_tester, 3> sv2{ 2 };
+         static_vector<copy_move_tester, 3> sv1{ 1, 3 };
+         static_vector<copy_move_tester, 3> sv2{ 2 };
          const auto* sv1_0 = sv1[0].addr();
          sv1 = sv2;
          REQUIRE(sv1.size() == 1);
@@ -120,8 +122,8 @@ TEST_CASE("assignment")
     }
     SUBCASE("move")
     {
-         dpm::static_vector<copy_move_tester, 3> sv1{ 1 };
-         dpm::static_vector<copy_move_tester, 3> sv2{ 2, 3 };
+         static_vector<copy_move_tester, 3> sv1{ 1 };
+         static_vector<copy_move_tester, 3> sv2{ 2, 3 };
          const auto* sv2_0 = sv2[0].addr();
          sv1 = std::move(sv2);
          REQUIRE(sv1.size() == 2);
@@ -130,13 +132,13 @@ TEST_CASE("assignment")
     }
     SUBCASE("assign(first, last)")
     {
-        dpm::static_vector<copy_move_tester, 3> sv1{ 1, 2, 3 };
-        dpm::static_vector<copy_move_tester, 3> sv2{ 4 };
+        static_vector<copy_move_tester, 3> sv1{ 1, 2, 3 };
+        static_vector<copy_move_tester, 3> sv2{ 4 };
         sv1.assign(sv2.begin(), sv2.end());
     }
     SUBCASE("assign(n, value)")
     {
-        dpm::static_vector<copy_move_tester, 3> sv1{ 1, 2 };
+        static_vector<copy_move_tester, 3> sv1{ 1, 2 };
         sv1.assign(3, 10);
         sv1.clear();
         sv1.assign(3, 20);
@@ -145,8 +147,8 @@ TEST_CASE("assignment")
 
 TEST_CASE("size/capacity")
 {
-    dpm::static_vector<int, 2> sv1;
-    dpm::static_vector<int, 2> sv2(2);
+    static_vector<int, 2> sv1;
+    static_vector<int, 2> sv2(2);
     REQUIRE(sv1.size() == 0);
     REQUIRE(sv2.size() == 2);
     REQUIRE(sv1.empty());
@@ -155,10 +157,10 @@ TEST_CASE("size/capacity")
     REQUIRE(sv2.capacity() == 2);
     REQUIRE(sv1.max_size() == sv1.capacity());
 
-    dpm::static_vector<copy_move_tester, 3> sv3{ 1, 2, 3 };
+    static_vector<copy_move_tester, 3> sv3{ 1, 2, 3 };
     sv3.resize(1);
     sv3.resize(3);
-    dpm::static_vector<copy_move_tester, 3> sv4;
+    static_vector<copy_move_tester, 3> sv4;
     sv4.resize(2);
 
     sv4.clear();
@@ -172,14 +174,14 @@ TEST_CASE("size/capacity")
 
 TEST_CASE("access")
 {
-    dpm::static_vector<int, 3> sv{ 1, 2, 3 };
+    static_vector<int, 3> sv{ 1, 2, 3 };
     REQUIRE(sv.front() == 1);
     REQUIRE(std::is_same_v<decltype(sv.front()), int&>);
 
     REQUIRE(sv.back() == 3);
     REQUIRE(std::is_same_v<decltype(sv.back()), int&>);
 
-    const dpm::static_vector<int, 3> const_sv{ 1, 2, 3 };
+    const static_vector<int, 3> const_sv{ 1, 2, 3 };
     REQUIRE(const_sv.front() == 1);
     REQUIRE(std::is_same_v<decltype(const_sv.front()), const int&>);
 
@@ -191,7 +193,7 @@ TEST_CASE("modifiers")
 {
     SUBCASE("clear")
     {
-        dpm::static_vector<constructor_count, 3> sv(3);
+        static_vector<constructor_count, 3> sv(3);
         REQUIRE(!sv.empty());
         REQUIRE(constructor_count::count > 0);
         sv.clear();
@@ -201,14 +203,14 @@ TEST_CASE("modifiers")
     // TODO: actually test these
     SUBCASE("emplace_back")
     {
-        dpm::static_vector<std::string, 3> sv;
+        static_vector<std::string, 3> sv;
         sv.emplace_back("hello");
         sv.emplace_back("world");
         sv.emplace_back(3, 'c');
     }
     SUBCASE("push_back")
     {
-        dpm::static_vector<copy_move_tester, 3> sv;
+        static_vector<copy_move_tester, 3> sv;
         sv.push_back(20);
 
         copy_move_tester value = 42;
@@ -218,7 +220,7 @@ TEST_CASE("modifiers")
     }
     SUBCASE("pop_back")
     {
-        dpm::static_vector<copy_move_tester, 3> v1;
+        static_vector<copy_move_tester, 3> v1;
         v1.push_back(20);
         v1.pop_back();
         v1.push_back(10);
@@ -226,8 +228,8 @@ TEST_CASE("modifiers")
     SUBCASE("swap")
     {
         {
-            dpm::static_vector<copy_move_tester, 3> sv1{ 1 };
-            dpm::static_vector<copy_move_tester, 3> sv2{ 2, 3 };
+            static_vector<copy_move_tester, 3> sv1{ 1 };
+            static_vector<copy_move_tester, 3> sv2{ 2, 3 };
 
             auto sv1_0 = sv1[0].value();
             auto sv2_0 = sv2[0].value();
@@ -242,8 +244,8 @@ TEST_CASE("modifiers")
             REQUIRE(sv2[0].value() == sv1_0);
         }
         {
-            dpm::static_vector<copy_move_tester, 3> sv1{ 1, 2 };
-            dpm::static_vector<copy_move_tester, 3> sv2{ 3 };
+            static_vector<copy_move_tester, 3> sv1{ 1, 2 };
+            static_vector<copy_move_tester, 3> sv2{ 3 };
 
             auto sv1_0 = sv1[0].value();
             auto sv1_1 = sv1[1].value();
@@ -259,8 +261,8 @@ TEST_CASE("modifiers")
             REQUIRE(sv2[1].value() == sv1_1);
         }
         {
-            dpm::static_vector<copy_move_tester, 3> sv1{ 1, 3 };
-            dpm::static_vector<copy_move_tester, 3> sv2{ 2, 4 };
+            static_vector<copy_move_tester, 3> sv1{ 1, 3 };
+            static_vector<copy_move_tester, 3> sv2{ 2, 4 };
 
             auto sv1_0 = sv1[0].value();
             auto sv1_1 = sv1[1].value();
@@ -281,19 +283,19 @@ TEST_CASE("modifiers")
     SUBCASE("insert")
     {
         {
-            dpm::static_vector<int, 5> sv{ 1, 2, 3 };
+            static_vector<int, 5> sv{ 1, 2, 3 };
             auto inserted = sv.insert(sv.begin() + 1, 5);
             REQUIRE(sv.size() == 4);
             REQUIRE(*inserted == 5);
         }
         {
-            dpm::static_vector<int, 5> sv{ 1, 2, 3 };
+            static_vector<int, 5> sv{ 1, 2, 3 };
             auto inserted = sv.insert(sv.begin(), 2, 7);
             REQUIRE(sv.size() == 5);
             REQUIRE(inserted == &sv[0]);
         }
         {
-            dpm::static_vector<copy_move_tester, 5> sv{ 1, 2 };
+            static_vector<copy_move_tester, 5> sv{ 1, 2 };
             std::array<copy_move_tester, 3> arr{ 3, 4, 5 };
             auto inserted = sv.insert(sv.begin() + 1, arr.begin(), arr.end());
             REQUIRE(sv.size() == 5);
@@ -303,14 +305,14 @@ TEST_CASE("modifiers")
     SUBCASE("erase")
     {
         {
-            dpm::static_vector<copy_move_tester, 3> sv{ 1, 2, 3 };
+            static_vector<copy_move_tester, 3> sv{ 1, 2, 3 };
             sv.erase(sv.begin() + 1);
             REQUIRE(sv.size() == 2);
             REQUIRE(sv[0].value() == 1);
             REQUIRE(sv[1].value() == 3);
         }
         {
-            dpm::static_vector<copy_move_tester, 3> sv{ 1, 2, 3 };
+            static_vector<copy_move_tester, 3> sv{ 1, 2, 3 };
             sv.erase(sv.begin(), sv.begin() + 2);
             REQUIRE(sv.size() == 1);
             REQUIRE(sv[0].value() == 3);
@@ -324,7 +326,7 @@ TEST_CASE("ranges")
 {
     SUBCASE("range-based for loop")
     {
-        dpm::static_vector<int, 5> sv(5, 2);
+        static_vector<int, 5> sv(5, 2);
         auto begin = sv.begin();
         auto end = sv.end();
         for (auto i : sv)
